53b841197f2612ec6385e2b313286baf6dc21608
suvo-khan <mdsuvoshek1@gmail.com>
2022-10-28 15:36:23 +0600

    added

--- index.js
+++ index.js
@@ -42,12 +42,43 @@ function changeSlide(){
     slides[index].style.display = "block";
     dots[index].classList.add("active");
 
-    index++;
-    setTimeout(changeSlide,5000);
-   
+    
+    
+    
+    
 
 }
-changeSlide();
 
 
+    changeSlide();
 
+    function changeSlide1(){
+    
+      if(index>slides.length-1)
+        index=0;
+      
+      if(index<0)
+        index=slides.length-1;
+      
+      
+      
+        for(let i=0;i<slides.length;i++){
+          slides[i].style.display = "none";
+          
+          dots[i].classList.remove("active");
+          
+          
+        }
+        
+        slides[index].style.display = "block";
+        dots[index].classList.add("active");
+    
+        
+        
+        index++;
+        
+    
+    }
+
+    setInterval(changeSlide1,3000);
+  
